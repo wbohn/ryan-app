b4abe7dd7a567a0a7ffb4e12463cc0e77a6d4c8a
DESKTOP-AVBRPII\PCENGINEER02 <rtimmons10987@gmail.com>
2023-05-17 10:37:18 -0700

    Changed message, Biscuits 4Ever

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, world!</p>
+        <p>Biscuits 4EVER!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
