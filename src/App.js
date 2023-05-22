8f3995cd5b13b06eb2febb19f4d8e94da6672191
William Bohn <william.bohn@corsosystems.com>
2023-05-22 10:09:08 -0500

    change image

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Biscuits 4EVER!</p>
-        <p>Biscuits 4EVER!</p>
-        <p>Biscuits 4EVER!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={process.env.PUBLIC_URL + "/tat.jpg"}
+          className="App-logo"
+          alt="logo"
+        />
       </header>
     </div>
   );
